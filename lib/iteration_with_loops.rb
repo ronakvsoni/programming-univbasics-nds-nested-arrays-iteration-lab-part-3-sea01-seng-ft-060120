2f8be2febc011b3e347169dc9697998dcfced7d4
Ronak V Soni <ronakvsoni@gmail.com>
2020-05-27 01:26:12 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,12 +7,13 @@ mixed_data = [
 def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
+  string = ""
     row_index = 0
   while row_index < src.count do
     element_index = 0
     while element_index < src[row_index].count do
       if src[row_index][element_index].class == String
-        p src[row_index][element_index]
+         src[row_index][element_index] + ""
       end
     element_index += 1
     end
